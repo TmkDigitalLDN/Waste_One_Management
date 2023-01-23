a998f464689f1151647ed49bad236ff17402fdd4
TmkDigitalLDN <88273097+TmkDigitalLDN@users.noreply.github.com>
2023-01-23 18:50:15 +0000

    latest

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,8 +1,8 @@
 <?php require("PHPMailer/PHPMailerAutoload.php");
 
 // ADD your Email and Name
-$recipientEmail='your@email';
-$recipientName='your Name';
+$recipientEmail='info@wasteone.co.uk';
+$recipientName='Waste One';
 
 //collect the posted variables into local variables before calling $mail = new mailer
 
